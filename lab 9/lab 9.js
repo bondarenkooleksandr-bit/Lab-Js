f1144508ec44c6fd25f464e96d905ddd36b00663
bondarenkooleksandr-bit <bondarenko.oleksandr@krfk.kai.edu.ua>
2026-06-25 23:38:55 +0300

    Create lab 9.js

---
+++ lab 9/lab 9.js
@@ -0,0 +1,8 @@
+// Стрілкова функція, яка повертає довжину рядка
+const getLength = (str) => str.length;
+
+// Виклик функції з різними рядками
+console.log(getLength("Hello"));        // 5
+console.log(getLength("JavaScript"));   // 10
+console.log(getLength("Привіт"));       // 6
+console.log(getLength(""));             // 0
